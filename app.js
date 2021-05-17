de2f71db38297e80458ac4c16f0de64dc18cb11d
MeikelFeel <pablo.bv.2004@gmail.com>
2021-05-16 21:18:49 -0300

    agregado firebase y login

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCg7jzGxnHjWFGQBqEgsWptM-ZAbIuUn8s",
+  authDomain: "fin-de-mes.firebaseapp.com",
+  databaseURL: "https://fin-de-mes-default-rtdb.firebaseio.com",
+  projectId: "fin-de-mes",
+  storageBucket: "fin-de-mes.appspot.com",
+  messagingSenderId: "635375528177",
+  appId: "1:635375528177:web:a0815f96303a547cefe150",
+  measurementId: "G-F9E4MWP3W0",
+};
+
+firebase.initializeApp(firebaseConfig);
+
 let monto = document.getElementById("monto");
-let gasto = document.getElementById("gasto");
-gasto.addEventListener("click", guardaRegistro);
+let categoria = document.getElementById("categoria");
+let guardar = document.getElementById("guardar");
+guardar.addEventListener("click", (e) => {
+  e.preventDefault();
+  guardaRegistro();
+});
 
 function guardaRegistro() {
   let valor = monto.value;
-  console.log(valor);
+  let cat = categoria.value;
+  console.log(cat);
+}
+
+let logGoogle = document.getElementById("login");
+logGoogle.addEventListener("click", (e) => {
+  e.preventDefault();
+  loguearConGoogle();
+});
+
+function loguearConGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => console.log("logueo"))
+    .catch((error) => console.log(error.message));
 }
