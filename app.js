4b117a75c2e9e3e604282db73833508d536b76bd
MeikelFeel <pablo.bv.2004@gmail.com>
2021-05-25 01:11:17 -0300

    Se agrega tabla con movimientos

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ let pic = document.getElementById("pic");
 let navLogin = document.getElementById("navLogin");
 let navLogout = document.getElementById("navLogout");
 let guardar = document.getElementById("guardar");
+let loginCard = document.getElementById("loginCard");
+let saldo = document.getElementById("saldo");
 
 guardar.addEventListener("click", (e) => {
   e.preventDefault();
@@ -31,15 +33,23 @@ navLogin.addEventListener("click", (e) => {
   loguearConGoogle();
 });
 
+navLogout.addEventListener("click", (e) => {
+  logout();
+});
+
 function guardaRegistro() {
   let valor = monto.value;
   let cat = categoria.value;
   mail = usuario.user.email;
+  let diaHora = Date.now();
+
+  console.log(diaHora, "desde guardar reg");
 
   const record = {
-    cuanto: valor,
+    cuanto: -valor,
     que: cat,
     quien: mail,
+    cuando: diaHora,
   };
 
   const db = firebase.database();
@@ -61,8 +71,71 @@ function loguearConGoogle() {
     .signInWithPopup(provider)
     .then((result) => {
       usuario = result;
-      user.innerHTML = usuario.user.displayName;
-      pic.src = usuario.user.photoURL;
+      infoUsuario();
     })
     .catch((error) => console.log(error.message));
+}
+
+function infoUsuario() {
+  user.innerHTML = usuario.user.displayName;
+  pic.src = usuario.user.photoURL;
+  fila1.style.display = "flex";
+  loginCard.style.display = "none";
+  navLogin.style.display = "none";
+  navLogout.style.display = "block";
+}
+
+function blanquearUser() {
+  user.innerHTML = "Iniciá Sesión";
+  pic.src = "./sources/descarga.png";
+  fila1.style.display = "none";
+  loginCard.style.display = "flex";
+  navLogout.style.display = "none";
+  navLogin.style.display = "block";
+}
+
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      blanquearUser();
+      console.log("Log Out ok");
+    })
+    .catch(function (error) {
+      console.log(error.message);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  if (user) {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        usuario = { user: user };
+        infoUsuario();
+      }
+    });
+  }
+});
+let resumen = 0;
+function calcularSaldo() {
+  const db = firebase.database();
+  const dbRef = db.ref("registros");
+  dbRef.on("child_added", (snapshot) => {
+    resumen = resumen + parseInt(snapshot.val().cuanto);
+    saldo.innerHTML = resumen;
+  });
+}
+calcularSaldo();
+
+function mostrarMovimientos() {
+  const db = firebase.database();
+  const dbRef = db.ref("registros");
+  dbRef.on("child_added", (snapshot) => {});
+}
+
+function obtenerFecha() {
+  const milisecs = Date.now();
+  ahora = new Date(milisecs);
+  return ahora;
 }
